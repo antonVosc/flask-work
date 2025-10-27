6dbd8ef84abebbe4ab1464d3d123310263c2009d
antonVosc <anton.vosc@gmail.com>
2025-10-27 23:38:57 +0300

    Worked with models and databases, created SQLAchemy market db instance

--- market.py
+++ market.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+
 db = SQLAlchemy(app)
 
 class Item(db.Model):
@@ -11,6 +13,9 @@ class Item(db.Model):
   barcode = db.Column(db.String(length=12), nullable=False, unique=True)
   description = db.Column(db.String(length=1024), nullable=False, unique=True)
 
+  def __repr__(self):
+    return f'Item {self.name}'
+
 @app.route('/')
 @app.route('/home')
 def home_page():
@@ -18,10 +23,6 @@ def home_page():
 
 @app.route('/market')
 def market_page():
-  items = [
-    {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
-    {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
-    {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
-  ]
+  items = Item.query.all()
 
   return render_template('market.html', items=items)
