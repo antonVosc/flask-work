93996cf6b748977dd0560b0495054cfdc9965f04
antonVosc <anton.vosc@gmail.com>
2025-10-31 17:22:10 +0300

    Working with Flask forms and implementing Flask validations

--- market/__init__.py
+++ market/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = 'ff666355fa40faf059e48762'
 
 db = SQLAlchemy(app)
 
