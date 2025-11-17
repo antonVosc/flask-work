b2f02c73af784ed2bc9fc1bf3340c83ecee9dc00
antonVosc <anton.vosc@gmail.com>
2025-11-17 20:36:34 +0300

    Added password hashing, wrote registration checks and implemented corrected login

--- market/routes.py
+++ market/routes.py
@@ -1,7 +1,8 @@
 from market import app, db
 from flask import render_template, redirect, url_for, flash
 from market.models import Item, User
-from market.forms import RegisterForm
+from market.forms import RegisterForm, LoginForm
+from flask_login import login_user
 
 @app.route('/')
 @app.route('/home')
@@ -19,7 +20,7 @@ def register_page():
   form = RegisterForm()
 
   if form.validate_on_submit():
-    user_to_create = User(username=form.username.data, email_address=form.email_address.data, password_hash=form.password1.data)
+    user_to_create = User(username=form.username.data, email_address=form.email_address.data, password=form.password1.data)
 
     db.session.add(user_to_create)
     db.session.commit()
@@ -31,3 +32,20 @@ def register_page():
       flash(f'There was an error with creating a user: {err_msg}', category='danger')
 
   return render_template('register.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login_page():
+  form = LoginForm()
+
+  if form.validate_on_submit():
+    attempted_user = User.query.filter_by(username=form.username.data).first()
+
+    if attempted_user and attempted_user.check_password_correction(attempted_password=form.password.data):
+      login_user(attempted_user)
+      flash(f'Success! Yo have now logged in as: {attempted_user.username}', category='success')
+      
+      return redirect(url_for('market_page'))
+    else:
+      flash('Username and password do not match! Please try again', category='danger')
+    
+  return render_template('login.html', form=form)
